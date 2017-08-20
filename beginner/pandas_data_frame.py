5c58e2a86f51cc233ca78fea1cff59af15f9dcf3
Nick Kondratiev <koteloks@yandex.ua>
2017-08-20 19:32:32 +0300

    Create pandas_data_frame.py

---
+++ beginner/pandas_data_frame.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(dict)
+print(cars)
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JAP', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars = pd.DataFrame(dict, index=row_labels)
+# Print cars again
+print(cars)
+
+
+# importing from csv
+# Import pandas as pd
+import pandas as pd
+
+# Fix import by including index_col
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out cars
+print(cars)
