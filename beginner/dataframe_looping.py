77551f9f500a43847a8817ecef0d7cd8bfe52dad
Nick Kondratiev <koteloks@yandex.ua>
2017-09-14 22:55:54 +0300

    Create dataframe_looping.py

---
+++ beginner/dataframe_looping.py
@@ -0,0 +1,20 @@
+# Iterating over numpy array
+# For loop over np_baseball
+for aa in np.nditer(np_baseball):
+    print(aa)
+    
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Iterate over rows of cars
+for lab, row in cars.iterrows():
+    print(lab)
+    print(row)
+    
+# Code for loop that adds COUNTRY column
+for lab, row in cars.iterrows():
+    cars.loc[lab, "COUNTRY"] = row["country"].upper()
+    
+# Use .apply(str.upper)
+cars["COUNTRY"] = cars["country"].apply(str.upper)
