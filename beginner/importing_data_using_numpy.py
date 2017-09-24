6cfb39d44ad0cb842bdaa75b39b4a4e5c7b225a6
Nick Kondratiev <koteloks@yandex.ua>
2017-09-24 16:43:10 +0300

    Create importing_data_using_numpy.py

---
+++ beginner/importing_data_using_numpy.py
@@ -0,0 +1,37 @@
+# Import package
+import numpy as np
+
+# Assign filename to variable: file
+file = 'digits.csv'
+
+# Load file as array: digits
+digits = np.loadtxt(file, delimiter=',')
+
+# Print datatype of digits
+print(type(digits))
+
+# Select and reshape a row
+im = digits[21, 1:]
+im_sq = np.reshape(im, (28, 28))
+
+# Plot reshaped data (matplotlib.pyplot already loaded as plt)
+plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.show()
+# ----------------------------------------------------------------------
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
+# ----------------------------------------------------------------------
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file, delimiter=',', names=True, dtype=None)
