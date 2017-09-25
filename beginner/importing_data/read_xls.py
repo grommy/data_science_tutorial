2a297c7689821459215255b5f7c6ba7300e048ee
Nick Kondratiev <koteloks@yandex.ua>
2017-09-25 10:11:30 +0300

    Update read_xls.py

--- beginner/importing_data/read_xls.py
+++ beginner/importing_data/read_xls.py
@@ -12,3 +12,27 @@ print(xl.sheet_names)
 
 # <script.py> output:
 #    ['2002', '2004']
+
+# Load a sheet into a DataFrame by name: df1
+df1 = xl.parse('2004')
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Load a sheet into a DataFrame by index: df2
+df2 = xl.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+# Parse the first sheet and rename the columns: df1
+df1 = xl.parse(0, skiprows=1, names=['Country','AAM due to War (2002)'])
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Parse the first column of the second sheet and rename the column: df2
+df2 = xl.parse(1, parse_cols=0, skiprows=1, names=['Country'])
+
+# Print the head of the DataFrame df2
+print(df2.head())
