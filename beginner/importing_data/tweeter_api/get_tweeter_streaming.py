3b2705f5f462fd63f0cfd344fa91685f239ab10b
Nick <koteloks@yandex.ua>
2017-10-16 16:19:36 +0300

    adjusting

--- beginner/importing_data/tweeter_api/get_tweeter_streaming.py
+++ beginner/importing_data/tweeter_api/get_tweeter_streaming.py
@@ -2,6 +2,11 @@
 import json
 import tweepy
 from tweeter_listener import MyStreamListener
+import re
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 # Store OAuth authentication credentials in relevant variables
 access_token = "808368154758021120-ZUiuPXLnufvCn25kP54dup04KVYGKhT"
@@ -21,6 +26,15 @@ l = MyStreamListener()
 stream = tweepy.Stream(auth, l)
 
 
+def word_in_text(word, tweet):
+    word = word.lower()
+    text = tweet.lower()
+    match = re.search(word, text)
+
+    if match:
+        return True
+    return False
+
 # Filter Twitter Streams to capture data by the keywords:
 track = stream.filter(track=['clinton', 'trump', 'sanders', 'cruz'])
 
@@ -32,3 +46,30 @@ with open(tweets_data_path, 'r') as tweets_file:
         tweets_data.append(tweet)
 
 print(tweets_data[0].keys())
+# Build DataFrame of tweet texts and languages
+df = pd.DataFrame(tweets_data, columns=['text', 'lang'])
+
+# Print head of DataFrame
+print(df.head())
+
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders += word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot histogram
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
