b0f78063974c77511a0da859ba6bf3d1515325e3
kondrati <kondratyev.n@gmail.com>
2017-12-20 11:17:03 +0200

    regression

--- statistical_thinking/regression/1.py
+++ statistical_thinking/regression/1.py
@@ -29,3 +29,22 @@ _ = plt.plot(x, y)
 
 # Draw the plot
 plt.show()
+
+
+# Optimizing RSS 
+# Specify slopes to consider: a_vals
+a_vals = np.linspace(0, 4, 200)
+
+# Initialize sum of square of residuals: rss
+rss = np.empty_like(a_vals)
+
+# Compute sum of square of residuals for each value of a_vals
+for i, a in enumerate(a_vals):
+    rss[i] = np.sum((fertility - a*illiteracy - b)**2)
+
+# Plot the RSS
+plt.plot(a_vals, rss, '-')
+plt.xlabel('slope (children per woman / percent illiterate)')
+plt.ylabel('sum of square of residuals')
+
+plt.show()
