c49662780231c885c6598b36fed908ff029c622a
kondrati <kondratyev.n@gmail.com>
2018-01-14 19:28:11 +0200

    supervised learning

--- ml_basics/supervised_learning/regression/fertility_and_life.py
+++ ml_basics/supervised_learning/regression/fertility_and_life.py
@@ -3,26 +3,77 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 # Read the CSV file into a DataFrame: df
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
 df = pd.read_csv('../DataSets/gm_2008_region.csv')
 
 # Create arrays for features and target variable
 y = df.life.values
-X = df.fertility.values
+X_fertility = df.fertility.values
 
 # Print the dimensions of X and y before reshaping
 print("Dimensions of y before reshaping: {}".format(y.shape))
-print("Dimensions of X before reshaping: {}".format(X.shape))
+print("Dimensions of X before reshaping: {}".format(X_fertility.shape))
 
 # Reshape X and y
 y = y.reshape(-1, 1)
-X = X.reshape(-1, 1)
+X_fertility = X_fertility.reshape(-1, 1)
 
 # Print the dimensions of X and y after reshaping
 print("Dimensions of y after reshaping: {}".format(y.shape))
-print("Dimensions of X after reshaping: {}".format(X.shape))
+print("Dimensions of X after reshaping: {}".format(X_fertility.shape))
 
 
-sns.heatmap(df.corr(), square=True, cmap='RdYlGn')
-plt.show()
+# sns.heatmap(df.corr(), square=True, cmap='RdYlGn')
+# plt.show()
+
+# Create the regressor: reg
+reg = LinearRegression()
+
+# Create the prediction space
+prediction_space = np.linspace(min(X_fertility), max(X_fertility)).reshape(-1, 1)
+
+# Fit the model to the data
+reg.fit(X_fertility, y)
+
+# Compute predictions over the prediction space: y_pred
+y_pred = reg.predict(prediction_space)
+
+# Print R^2
+print(reg.score(X_fertility, y))
+
+# Plot regression line
+# plt.scatter(X_fertility, y)
+# plt.plot(prediction_space, y_pred, color='black', linewidth=3)
+# plt.show()
+
+
+# Make more sophisticated model and look on R^2. It will never decrease  unless you use adjusted R^2
+df_x = df.copy()
+del df_x['life']
+del df_x['Region']
+
+print(df_x.info())
+X = df_x.values
+print(X.shape)
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create the regressor: reg_all
+reg_all = LinearRegression()
+
+# Fit the regressor to the training data
+reg_all.fit(X_train, y_train)
+
+# Predict on the test data: y_pred
+y_pred = reg_all.predict(X_test)
+
+# Compute and print R^2 and RMSE
+print("R^2: {}".format(reg_all.score(X_test, y_test)))
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print("Root Mean Squared Error: {}".format(rmse))
